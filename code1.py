43cea06e1cf6045a9c27b55f9a3755f4b8588df8
Akhilesh <deepakkushwaha6392@gmail.com>
2024-03-31 12:54:28 +0530

    second commit

--- code1.py
+++ code1.py
@@ -1,7 +1,11 @@
 # practicing various git comamnds
+from code2 import multi
 def cal(n,m):
     sum=n+m
     return sum 
 
 ans=cal(10,12)
 print(ans)
+ans2=multi(19,10)
+print(ans2)
+
---
+++ code2.py
@@ -0,0 +1,6 @@
+def multi(m,n):
+    sum=m*n
+    return sum
+
+
+ans=multi(10,10)
